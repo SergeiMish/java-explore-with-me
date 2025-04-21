b86559f0b55f39f6296e2d572c1667d0c7d86bdd
Sergei Mishalevskii <108402627+SergeiMish@users.noreply.github.com>
2025-04-21 10:24:19 +0500

    diploma - main service (all done)

--- stats/server/src/main/java/ru/practicum/config/WebConfig.java
+++ stats/server/src/main/java/ru/practicum/config/WebConfig.java
@@ -1,17 +1,10 @@
 package ru.practicum.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.MethodParameter;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.support.WebDataBinderFactory;
-import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Configuration
@@ -24,30 +17,5 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new CustomDateResolver());
-    }
-}
-
-@Component
-class CustomDateResolver implements HandlerMethodArgumentResolver {
-
-    @Override
-    public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.getParameterType().equals(LocalDateTime.class);
-    }
-
-    @Override
-    public Object resolveArgument(MethodParameter parameter,
-                                  ModelAndViewContainer mavContainer,
-                                  NativeWebRequest webRequest,
-                                  WebDataBinderFactory binderFactory) throws Exception {
-        String dateStr = webRequest.getParameter(parameter.getParameterName());
-        if (dateStr == null) return null;
-
-        try {
-            return LocalDateTime.parse(dateStr.replace("%20", " "),
-                    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Invalid date format");
-        }
     }
 }
